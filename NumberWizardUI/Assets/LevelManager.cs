886e14640228416059913fa5efb96ad9cc724e17
Frederico Simas Duarte <fredmdfk@gmail.com>
2017-01-02 22:43:53 -0400

    Add events for Scenes Buttons (Start, Quit and Return)
    	- Copied content from Start scene to Game scene;
    	- Leave only one button on Game Return with text 'Return';
    	- Add events for Start and Quit Buttons: Application.LoadlLevel()
    	  * and Application.Quit();
    	- On Game scene, Return button gos back to Start Scene;
    
    Signed-off-by: Frederico Simas Duarte <fredmdfk@gmail.com>

--- NumberWizardUI/Assets/LevelManager.cs
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -5,9 +5,11 @@ public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(string name) {
         Debug.Log("Level Load requested for " + name);
+        Application.LoadLevel(name);
     }
 
     public void QuitRequest() {
         Debug.Log("I want to Quit!");
+        Application.Quit();
     }
 }
