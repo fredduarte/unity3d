53af60552e41cdf8aaca83aca4c3a7ab965f952b
Frederico Simas Duarte <fredmdfk@gmail.com>
2017-01-10 20:11:19 -0400

    Added movement to the ball
    	- Created script to move the ball with the input arrows
    	  of the keyboard;
    
    Signed-off-by: Frederico Simas Duarte <fredmdfk@gmail.com>

---
+++ tutorials/roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody mRigidbody;
+
+    void Start() {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        mRigidbody.AddForce(movement * speed);
+    }
+}
